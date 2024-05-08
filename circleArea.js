f6467e799899a9f83bae567f502ff0619aa89e06
RafetTanriverdi <by.rafet.tanriverdi@gmail.com>
2024-05-08 23:14:23 +0300

    circleArea math

---
+++ circleArea.js
@@ -0,0 +1,8 @@
+const arguments = process.argv.slice(2);
+
+function CircleArea(radius) {
+  let Area= Math.PI * radius * radius;
+  let result = Area.toFixed(2);
+  return result;
+}
+console.log('Radius: ',arguments[0],',','Area:',CircleArea(arguments[0])); // 153.93804002589985
