a6a1d206799888af166a5ade96baef0f39716e5a
RafetTanriverdi <by.rafet.tanriverdi@gmail.com>
2024-05-09 22:30:41 +0300

    new folder

--- Async/promise.js
+++ Async/promise.js
@@ -1,13 +1,67 @@
-const promise1 = new Promise((resolve, reject) => {
-  resolve("Promise 1 is resolved");
-  reject('Promise 1 is rejected');
-});
+// const promise1 = new Promise((resolve, reject) => {
+//   resolve("Promise 1 is resolved");
+//   reject('Promise 1 is rejected');
+// });
 
-console.log(promise1);
+// console.log(promise1);
 
-promise1
+// promise1
+//   .then((value) => {
+//     console.log(value);
+//   })
+//   .catch((error) => {
+//     console.log(error);
+//   });
+
+const books = [
+  {
+    author: "J.K. Rowling",
+    book: "Harry Potter and the Sorcerer's Stone",
+  },
+
+  {
+    author: "J.K. Rowling",
+    book: "Harry Potter and the Chamber of Secrets",
+  },
+  {
+    author: "J.K.Tolkien",
+    book: "The Lord of the Rings",
+  },
+  {
+    author: "J.K.Tolkien",
+    book: "The Hobbit",
+  },
+  {
+    author: "Arthur C. Clarke",
+    book: "2001: A Space Odyssey",
+  },
+  {
+    author: "Carl Sagan",
+    book: "Cosmos",
+  },
+];
+
+const listBooks = () => {
+  books.map((book) => {
+    console.log(book.book + " by " + book.author);
+  });
+};
+
+const addBook = (newBook) => {
+  const promise1 = new Promise((resolve, reject) => {
+    resolve(books);
+    books.push(newBook);
+  });
+  return promise1;
+};
+
+addBook({
+  author: "Douglas Adams",
+  book: "The Hitchhiker's Guide to the Galaxy",
+})
   .then((value) => {
-    console.log(value);
+    console.log("new list ");
+    listBooks();
   })
   .catch((error) => {
     console.log(error);
